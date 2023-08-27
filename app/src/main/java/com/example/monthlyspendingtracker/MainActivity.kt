4d34f60b27ce2876778ebae84bf145eee52fc2f1
Daniel Howard <acrodemocide@hotmail.com>
2023-08-27 09:40:24 -0600

    Removed unused imports

--- app/src/main/java/com/example/monthlyspendingtracker/MainActivity.kt
+++ app/src/main/java/com/example/monthlyspendingtracker/MainActivity.kt
@@ -3,35 +3,12 @@ package com.example.monthlyspendingtracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.room.Room
-import com.example.monthlyspendingtracker.data.AppDatabase
-import com.example.monthlyspendingtracker.data.ExpenseEntity
 import com.example.monthlyspendingtracker.ui.theme.MonthlySpendingTrackerTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.text.NumberFormat
-import java.util.Calendar
-import java.util.Currency
-import java.util.Date
-import androidx.compose.material.ModalDrawer
 import com.example.monthlyspendingtracker.screens.HomeScreen
 
 //enum class Category(val label: String, val icon: Unit) {
