0d02f8332fb2bc4a29b78d355a40c5101d62b86b
Daniel Howard <acrodemocide@hotmail.com>
2023-09-03 11:20:08 -0600

    Added in default JetPack Compose code for Alert Dialog to start filling in with UI elements to edit an entry when selected from list of history items

--- app/src/main/java/com/example/monthlyspendingtracker/screens/HistoryScreen.kt
+++ app/src/main/java/com/example/monthlyspendingtracker/screens/HistoryScreen.kt
@@ -1,13 +1,28 @@
 package com.example.monthlyspendingtracker.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -19,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.room.Room
 import com.example.monthlyspendingtracker.R
@@ -30,6 +46,7 @@ import java.text.NumberFormat
 import java.util.Currency
 //import androidx.compose.material3.HorizontalDivider
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HistoryScreen () {
     val database = Room.databaseBuilder(
@@ -38,20 +55,76 @@ fun HistoryScreen () {
         "expenses-db"
     ).build()
 
-    val currentMonth = getFirstOfCurrentMonth()
+    val startOfCurrentMonth = getFirstOfCurrentMonth()
 
     var expenses: List<ExpenseEntity> by remember { mutableStateOf(emptyList<ExpenseEntity>()) }
 
     LaunchedEffect(Unit) {
         val dbExpenses = withContext(Dispatchers.IO) {
-            database.expenseDao().getExpensesForMonth(currentMonth) ?: emptyList()
+            database.expenseDao().getExpensesForMonth(startOfCurrentMonth) ?: emptyList()
         }
         expenses = dbExpenses
     }
 
+    var purchaseAmount by remember { mutableStateOf("") }
     val format: NumberFormat = NumberFormat.getCurrencyInstance()
     format.maximumFractionDigits = 2
     format.currency = Currency.getInstance("USD")
+
+    val openDialog = remember { mutableStateOf(false) }
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openDialog.value = false
+            }
+        ) {
+            Surface(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                shape = MaterialTheme.shapes.large,
+                tonalElevation = AlertDialogDefaults.TonalElevation
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "This area typically contains the supportive text " +
+                                "which presents the details regarding the Dialog's purpose.",
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+                    // Text box to edit purchase amount
+                    OutlinedTextField(
+                        value = purchaseAmount,
+                        onValueChange = { purchaseAmount = it },
+                        label = { Text("Enter purchase price (USD)") },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        leadingIcon = { Icon(painterResource(R.drawable.ic_baseline_attach_money_24), contentDescription = null) },
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = OutlinedTextFieldDefaults.colors(focusedTextColor = Color.Black, unfocusedTextColor = Color.Black)
+                    )
+                    TextButton(
+                        onClick = {
+                            openDialog.value = false
+                        },
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text("Cancel")
+                    }
+                    TextButton(
+                        onClick = {
+                            openDialog.value = false
+                        },
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text("Save")
+                    }
+                }
+            }
+        }
+    }
 
     LazyColumn(
         modifier = Modifier
@@ -86,7 +159,10 @@ fun HistoryScreen () {
                         overlineColor = Color.DarkGray,
                         supportingColor = Color.Gray,
                         trailingIconColor = Color.Gray
-                    )
+                    ),
+                    modifier = Modifier.clickable {
+                        openDialog.value = true
+                    }
                 )
             }
             //        HorizontalDivider()
